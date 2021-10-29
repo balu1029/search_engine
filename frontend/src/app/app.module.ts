1558320b5c662f02aed7c5a27fd1f4d0bfa396c3
Josh <57182684+Zojo1012@users.noreply.github.com>
2021-10-29 15:17:22 +0200

    added reactive form to send to backend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-search-bar',
@@ -7,15 +9,46 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
-  faSearch = faSearch;
+  
+  backendUrl: string = 'http://34.198.77.180:5000/search';
+  fireballURL: string = 'https://www.dnd5eapi.co/api/spells/fireball'
 
-  constructor() { }
+  faSearch = faSearch;
+  
+  beerForm!: FormGroup;
+  formForBackend: any;
+
+  searchResults;
+
+  constructor(private fb: FormBuilder, 
+              private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.initForm();
+
+    this.beerForm.valueChanges.subscribe(console.log)
   }
 
-  search(searchRequest: string) {
-
+  initForm() {
+    this.beerForm = this.fb.group({
+      query: '',
+      minABV: '',
+      maxABV: '',
+      brewMethod: ''
+    })
   }
 
+  onSubmit() {
+    this.formForBackend = this.beerForm;
+    console.log(this.formForBackend);
+    //this.sendToBackend();
+  }
+
+  sendToBackend() {
+    return new Promise(resolve => {
+      this.http.post(this.backendUrl, this.formForBackend).toPromise().then((res) => {
+        this.searchResults = res;
+      });
+    });
+  }
 }
