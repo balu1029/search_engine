b25446cbada5860a62ce9150d4df6c3bf48bbc2f
Lukas Baer <balu1029@hs-karlsruhe.de>
2021-10-30 16:14:00 +0200

    added /autocomplete and /brew. both working so far

--- backend/src/restful_endpoint.py
+++ backend/src/restful_endpoint.py
@@ -3,12 +3,13 @@ from flask.globals import request
 from elasticsearch import Elasticsearch
 import json
 from flask_cors import CORS, cross_origin
-from elasticsearch_dsl import Search, Q
+from elasticsearch_dsl import Search, Q, A
 from elasticsearch_dsl.query import Match
 
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
+base_url = "https://www.brewersfriend.com"
 
 es = Elasticsearch(
     "http://node-2.hska.io:9200/"
@@ -32,27 +33,27 @@ def search():
     res = []
     req = json.loads(request.data)
 
-    s = Search(using = es)
+    s = Search(using = es, index = "beer_recipes")
     query = Q("match", Name = req["query"]) | Q("match", Style = req["query"])
     s = s.query(query)
 
     try:
-        if req["minABV"] == "":
-            req["minABV"] = 0
+        if req["extraOptions"]["minABV"] == "":
+            req["extraOptions"]["minABV"] = 0
     except:
-        req["minABV"] = 0  
+        req["extraOptions"]["minABV"] = 0  
     try:
-        if req["maxABV"] == "":
-            req["maxABV"] = 100
+        if req["extraOptions"]["maxABV"] == "":
+            req["extraOptions"]["maxABV"] = 100
     except:
-        req["maxABV"] = 100
+        req["extraOptions"]["maxABV"] = 100
     try:
-        if req["brewMethod"] != "":
-            s = s.filter("term", **{"BrewMethod" : req["brewMethod"]})
+        if req["extraOptions"]["brewMethod"] != "":
+            s = s.filter("term", **{"BrewMethod" : req["extraOptions"]["brewMethod"]})
     except:
-        req["brewMethod"] = ""
+        req["extraOptions"]["brewMethod"] = ""
 
-    s = s.filter("range", **{"ABV": {"from": req["minABV"], "to": req["maxABV"]}})
+    s = s.filter("range", **{"ABV": {"from": req["extraOptions"]["minABV"], "to": req["extraOptions"]["maxABV"]}})
     
     es_res = s.execute()
 
@@ -60,7 +61,7 @@ def search():
         try:
             response = {}
             response["name"] = hit.Name
-            response["url"] = hit.URL
+            response["url"] = base_url + hit.URL
             response["abv"] = hit.ABV
             response["brewMethod"] = hit.BrewMethod
             response["style"] = hit.Style
@@ -71,10 +72,65 @@ def search():
         
     return json.dumps(res)
 
+
+#path for autocompletion proposes
 @app.route("/auto",methods = ['GET'])
 @cross_origin()
 def auto():
 
+    return "done"
+
+
+@app.route("/brew",methods = ['GET'])
+@cross_origin()
+def brew():
+    s = Search(using = es, index = "beer_recipes")
+    aggregation = A("terms", field = "BrewMethod")
+    s.aggs.bucket("dist_brewmethods", aggregation)
+    es_res = s.execute()
+
+    methods = []
+    res = {}
+
+    for elem in es_res["aggregations"]["dist_brewmethods"]["buckets"]:
+        methods.append(elem["key"])
+    res["results"] = methods
+    return json.dumps(res)
+
+@app.route("/autocomplete",methods = ['GET'])
+@cross_origin()
+def autocomplete():
+
+    q = request.args.get("query")
+    s = Search(using = es, index = "beer_recipes")
+
+    query = Q("match", Name = q)
+    s = s.query(query)
+    es_res = s.execute()
+
+    methods = []
+    res = {}
+    count = 0
+    for hit in es_res:
+        if str(q) in str(hit["Name"]):
+            val = hit["Name"]
+        elif str(q) in str(hit["Style"]):
+            val = hit["Style"]
+        else:
+            continue
+
+        if str(val) == str(q):
+            continue
+        
+        if count < 7:
+            if val not in methods:
+                methods.append(val)
+                count = count + 1
+        else:
+            break
+        
+    res["results"] = methods
+    return json.dumps(res)
 
 app.run(host = "0.0.0.0")
 
