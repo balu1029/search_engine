ff7ba2e78b9aeb86c274cc8cf3c000f34d455cb0
Josh <57182684+Zojo1012@users.noreply.github.com>
2021-10-30 16:15:06 +0200

    added styles to search input form and results, added autocompletion suggestions

--- frontend/src/app/search-bar/search-bar.component.ts
+++ frontend/src/app/search-bar/search-bar.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import { faSearch, faCaretRight } from '@fortawesome/free-solid-svg-icons';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
@@ -10,45 +10,79 @@ import { HttpClient } from '@angular/common/http';
 })
 export class SearchBarComponent implements OnInit {
   
-  backendUrl: string = 'http://34.198.77.180:5000/search';
+  backendUrl: string = 'http://34.198.77.180:5000/';
   fireballURL: string = 'https://www.dnd5eapi.co/api/spells/fireball'
 
   faSearch = faSearch;
+  faCaretRight = faCaretRight;
   
   beerForm!: FormGroup;
-  formForBackend: any;
+  formValues;
+  brewMethods;
+  autoCompleteSugg = [];
 
   searchResults;
+  extraOptionsToggle = false;
+  
+
 
   constructor(private fb: FormBuilder, 
               private http: HttpClient) { }
 
   ngOnInit(): void {
     this.initForm();
+    this.lookUpBrewMethods();
+    document.getElementById('searchInput').addEventListener('input', this.updateValue)
+  }
 
-    this.beerForm.valueChanges.subscribe(console.log)
+  updateValue = (event) => {
+    this.getAutoCompleteSuggestions(event.target.value);
   }
 
   initForm() {
     this.beerForm = this.fb.group({
       query: '',
-      minABV: '',
-      maxABV: '',
-      brewMethod: ''
+      extraOptions: this.fb.group({
+        minABV: '',
+        maxABV: '',
+        brewMethod: ''
+      }),
     })
   }
 
   onSubmit() {
-    this.formForBackend = this.beerForm;
-    console.log(this.formForBackend);
-    //this.sendToBackend();
+    this.formValues = this.beerForm.value;
+    console.log(this.formValues);
+    this.sendToBackend();
   }
 
   sendToBackend() {
     return new Promise(resolve => {
-      this.http.post(this.backendUrl, this.formForBackend).toPromise().then((res) => {
+      this.http.post(this.backendUrl + 'search', this.formValues).toPromise().then((res) => {
+        console.log(res);
         this.searchResults = res;
       });
     });
   }
+
+  lookUpBrewMethods() {
+    return new Promise(resolve => {
+      this.http.get(this.backendUrl + 'brew').toPromise().then((res) => {
+        this.brewMethods = res;
+      });
+    });
+  } 
+
+  openExtraOptions() {
+    this.extraOptionsToggle = !this.extraOptionsToggle;
+  }
+
+  getAutoCompleteSuggestions(value) {
+    return new Promise(resolve => {
+      this.http.get(this.backendUrl + 'autocomplete?query=' + value).subscribe((res: any) => {
+        this.autoCompleteSugg = res.results;
+        console.log(res.results);
+      });
+    });
+  } 
 }
