e9d19c044a5e7d1e275e040b41e5ce8cc00a0ca7
Lukas Baer <balu1029@hs-karlsruhe.de>
2021-10-30 09:36:15 +0200

    restful api works on /search

--- backend/src/restful_endpoint.py
+++ backend/src/restful_endpoint.py
@@ -1,5 +1,7 @@
 from flask import Flask
+from flask.globals import request
 from elasticsearch import Elasticsearch
+import json
 
 app = Flask(__name__)
 
@@ -11,8 +13,18 @@ es = Elasticsearch(
 def hello_world():
     return "<p>Hello, World!</p>"
 
+# send query via: .../search?query=<insert query here>
+@app.route("/search")
+def search():
+    '''
+    q = request.args.get('query')
+    q = json.loads(q)
+    res = es.search(index="beer_recipes", query=q)
+    '''
+    print(request.data)
+    return "done"
 
 
-res = es.search(index="beer_recipes", query={"range":{"ABV":{"gte":10}}})
-print(res["hits"]["hits"][-1]["_source"]["ABV"])
-#app.run()
+app.run(host = "0.0.0.0")
+
+# wie viele verschiedenen brew methods
